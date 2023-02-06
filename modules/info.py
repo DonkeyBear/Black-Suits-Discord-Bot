93526c1d0decc757ef47a4755b8ff26f3e6d0856
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2023-02-07 05:43:30 +0800

    Update info.py

--- modules/info.py
+++ modules/info.py
@@ -1,9 +1,10 @@
 def getHelp():
     commands = [
         {'command': '-help', 'desc': '開啟指令說明。'},
-        {'command': '-dice', 'desc': '輸入 `-dice <數字>` 即可生成相應範圍內的隨機數。'},
-        {'command': '-meme', 'desc': '從好色龍的網誌隨機回傳一張趣圖。'},
-        {'command': '-idiom', 'desc': '隨機回傳一則成語，並附上注音、釋義及典故。'},
+        {'command': '-dice <整數>', 'desc': '生成相應範圍內的隨機數。'},
+        {'command': '-fun', 'desc': '從好色龍的網誌隨機回傳一張趣圖。'},
+        {'command': '-idiom', 'desc': '隨機回傳一則成語，並附上詳細說明。'},
+        {'command': '-idiom <文字>', 'desc': '根據文字內容進行搜尋，回傳一則成語或熟語，並附上詳細說明。'},
         {'command': '-wiki', 'desc': '隨機回傳一則維基百科條目，節錄條目中的第一段摘要。'}
     ]
 
